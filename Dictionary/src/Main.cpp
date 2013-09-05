b0c4cde4cb77cba3e8d719c2b3e4276314d146e5
Abil N George <abilngeorge@gmail.com>
2013-09-05 13:24:08 +0000

    Assignment 2
    git-svn-id: https://subversion.assembla.com/svn/cs6140@15 dcdcd5bf-5c71-4fd8-8603-51941cb4daa4

--- Dictionary/src/Main.cpp
+++ Dictionary/src/Main.cpp
@@ -5,11 +5,38 @@
  *      Author: abilng
  */
 
+//#include "Dictionary.h"
+//#include "BSTree.h"
+//#include "RBTree.h"
+#include <ctime>
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+
+void help(char * prgname)
+{
+	cout<<"Usage: "<<prgname<<" [-bst][-t] insertFile searchFile";
+	cout<<" [ITimeFile] [STimeFile] "<<endl;
+
+	cout<<endl;
+	cout<<"-bst		Use Binary Search Tree"<<endl;
+	cout<<"		  if this flag is not present,";
+	cout<<" program will use Red Black Tree"<<endl;
+	cout<<"-t		Calculate time for insert and lookup"<<endl;
+	cout<<"		  timing in appended in `ITimeFile` `STimeFile` respectively "<<endl;
+	cout<<endl;
+}
+
+
 int main(int argc, char*argv[])
 {
+	if(argc == 1) help(argv[0]);
 
 	//TODO
 
+	return 0;
 }
 
 

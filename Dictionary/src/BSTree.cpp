23c53359c9fb5fbc950fd9e45b0ce7fb8016134f
Aashima Bhatia <aashima91@gmail.com>
2013-09-07 15:11:55 +0000

    insert
    git-svn-id: https://subversion.assembla.com/svn/cs6140@25 dcdcd5bf-5c71-4fd8-8603-51941cb4daa4

--- Dictionary/src/BSTree.cpp
+++ Dictionary/src/BSTree.cpp
@@ -9,19 +9,57 @@
 
 BSTree::BSTree()
 {
-	// TODO Auto-generated constructor stub
+	nill = new BSTreeNode();
+		nill->val = NA;
+		nill->left = NULL;
+		nill->right= NULL;
+		nill->p = NULL;
+
+			root = nill;
 
 }
 
 BSTree::~BSTree()
 {
-	// TODO Auto-generated destructor stub
+	clear();
+		delete nill;
 }
 
 void BSTree::insert(data val)
 {
-	//TODO
+	BSTreeNode *x, *y, *z;
+	z = new BSTreeNode();
+		z->val = val;
+		z->left = this->nill;
+		z->right = this->nill;
+		z->p = this->nill;
+		y = this->nill;
+			x = this->root;
+			while(x != this->nill)
+			{
+				y = x;
+				if(z->val < x->val)
+					x = x ->left;
+				else
+					x = x->right;
+			}
+			z->p = y;
+			if(y == this->nill)
+					this->root = z;
+				else
+				{
+					if(z->val < y->val)
+						y->left = z;
+					else
+						y->right = z;
+				}
+				z->left = this->nill;
+				z->right = this->nill;
+		  }
+
 }
+
+
 
 bool BSTree::del(data val)
 {
